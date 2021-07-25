9dd81ac604bb1e54350a6cc772be20d5d7d383f7
Nierot <56843328+Nierot@users.noreply.github.com>
2021-07-25 16:50:15 +0200

    ✨ Added the host and need house pages

---
+++ src/pages/HostHouse.js
@@ -0,0 +1,11 @@
+const HostHouse = ({ lang: { pages: { haveHouse } } }) =>
+<div className="h-screen px-6 py-3">
+  <div>
+    { haveHouse.explanation }
+  </div>
+  <a href="https://docs.google.com/forms/d/e/1FAIpQLSc57U37lhETv9PMaqISVqPwJYIqIWqeRS1C-L_hqj9P53jvMA/viewform?usp=sf_link">
+    { haveHouse.link }
+  </a>
+</div>
+
+export default HostHouse
---
+++ src/pages/NeedHouse.js
@@ -0,0 +1,11 @@
+const NeedHouse = ({ lang: { pages: { needHouse } } }) =>
+<div className="h-screen px-6 py-3">
+  <div>
+    { needHouse.explanation }
+  </div>
+  <a href="https://docs.google.com/forms/d/e/1FAIpQLSc57U37lhETv9PMaqISVqPwJYIqIWqeRS1C-L_hqj9P53jvMA/viewform?usp=sf_link">
+    { needHouse.link }
+  </a>
+</div>
+
+export default NeedHouse
