8926ef8dc6bfe760de2beb7377a39e2dd0e6608d
Nierot <56843328+Nierot@users.noreply.github.com>
2021-07-29 11:50:31 +0200

    ✨ Add privacy page

--- src/pages/Privacy.js
+++ src/pages/Privacy.js
@@ -1,6 +1,11 @@
 const Privacy = ({ lang: { pages: { privacy } } }) =>
-<div className="h-5/6 px-6 py-3 bg-tertiary">
-  Privacy
+<div className="h-5/6 px-6 py-6 bg-tertiary">
+  <p className="">
+    { privacy.explanation }
+  </p>
+  <a href="/privacy_statement_SOS_20210728.pdf" className="pt-2 text-secondary underline">
+    { privacy.link }
+  </a>
 </div>
 
 export default Privacy
