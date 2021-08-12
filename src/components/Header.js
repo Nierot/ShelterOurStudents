ffa4205272edd4d4d976040326c5b3219afe3f22
Nierot <git@nierot.com>
2021-08-12 11:31:50 +0200

    ✨ Add logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,15 +3,17 @@ import { MenuIcon, XIcon } from '@heroicons/react/solid'
 const Header = ({ lang, toggle, open }) => {
   return (
     <div>
-      <header id="header" className="w-full h-16 grid grid-cols-6 bg-primary">
-        <a className="col-span-5" href="/">
-          Logo
-        </a>
+      <header id="header" className="w-full h-16 grid grid-cols-6 bg-secondary">
+        <div className="col-span-5">
+          <a href="/">
+            <img className="h-16" src="/logo_text.svg" alt="Shelter Our Students" />
+          </a>
+        </div>
         <div className="col-span-1 flex flex-row-reverse justify-start space-x-1" onClick={() => toggle()}>
           <div className="w-3"></div>
           {open ?
-              <XIcon className="w-10" /> :
-              <MenuIcon className="w-12"/>
+              <XIcon className="w-10 fill-current text-primary" /> :
+              <MenuIcon className="w-12 fill-current text-primary" />
           }
         </div>
       </header>
